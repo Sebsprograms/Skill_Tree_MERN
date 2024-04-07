2f2cac46263a48c6405c9080153edb6ba8e5b686
Sebastian Benad <sebastian@Sebastians-MacBook-Pro.local>
2024-04-07 12:34:14 -0600

    Finish skeleton of site and implement menu skeleton

---
+++ client/src/pages/SkillTree.jsx
@@ -0,0 +1,11 @@
+const SkillTree = () => {
+    return (
+        <div>
+            <h1>Skill 1</h1>
+            <h1>Skill 2</h1>
+            <h1>Skill 3</h1>
+        </div>
+    );
+}
+
+export default SkillTree;
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,2 +1,3 @@
 export { default as Layout } from './Layout';
 export { default as Landing } from './Landing';
+export { default as SkillTree } from './SkillTree';
